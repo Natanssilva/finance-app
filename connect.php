6f6dcdb05318802eb06192d0f5d3383a194e2f5f
Natanssilva <natanssilva10@gmail.com>
2023-10-12 18:07:48 -0300

    tela de login e cadastro

--- connect.php
+++ connect.php
@@ -1,4 +1,13 @@
 <?php
 
+$database = 'myfinance';
+$user = 'root';
+$password = '';
 
+$ligacao = new PDO("mysql:host=localhost,dbname=$database",$user,$password);
 
+try {
+    //code...
+} catch (\Throwable $th) {
+    //throw $th;
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-800 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded shadow-md sm:w-96 animate__animated animate__backInRight ">
+        <h2 class="text-2xl font-bold mb-4">Login</h2>
+        <form>
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Email</label>
+                <input type="email" id="email" name="email" class="border rounded w-full py-2 px-3" placeholder="seuemail@example.com">
+            </div>
+            <div class="mb-6">
+                <label for "senha" class="block text-gray-700 text-sm font-semibold mb-2">Senha</label>
+                <input type="password" id="senha" name="senha" class="border rounded w-full py-2 px-3" placeholder="********">
+            </div>
+            <button type="submit" class="bg-blue-500 text-white rounded w-full py-2">Entrar</button>
+        </form>
+        <div class="mt-4">
+            <p class="text-gray-600">Novo por aqui? <a href="cadastro.php" class="text-blue-500">Crie uma conta agora</a></p>
+        </div>
+    </div>
+
+    <h1 class="text-2xl text-white text-center absolute top-20 w-full animate__animated animate__backInRight ">Seja bem-vindo ao Finance App, o lugar certo para suas finanças !</h1>
+</body>
+</html>
