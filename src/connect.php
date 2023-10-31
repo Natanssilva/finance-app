a4ea0ba0316b285481f45c87743e62f0c7fc67c4
Natanssilva <natanssilva10@gmail.com>
2023-10-30 22:13:47 -0300

    correcoes

--- src/connect.php
+++ src/connect.php
@@ -1,39 +1,9 @@
 <?php
+
 session_start();
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require __DIR__ . '/../vendor/autoload.php';
-// Crie uma instância do PHPMailer
-// $mail = new PHPMailer(true);
 
-// try {
-//     // Configurações do servidor SMTP
-//     $mail->isSMTP();
-//     $mail->Host = 'smtp.gmail.com';
-//     $mail->SMTPAuth = true;
-//     $mail->Username = 'natanssilva10@gmail.com';
-//     $mail->Password = 'moyp edpa ogof bqyt';
-//     $mail->SMTPSecure = 'tls';  // Use 'tls' para o Gmail
-//     $mail->Port = 587;  // Porta SMTP padrão do Gmail
-
-
-//     // Remetente e destinatário
-//     $mail->setFrom('natanssilva10@gmail.com', 'me');
-//     $mail->addAddress('natanssilva10@gmail.com', 'Natan');
-
-//     // Conteúdo do e-mail
-//     $mail->isHTML(true);
-//     $mail->Subject = 'Assunto do E-mail';
-//     $mail->Body = 'Este é um e-mail de teste enviado com o PHPMailer.';
-
-//     // Envie o e-mail
-//     $mail->send();
-//     echo 'E-mail enviado com sucesso.';
-// } catch (Exception $e) {
-//     echo 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
-// }
 
 $host = 'localhost';
 $database = 'myfinance';
--- src/processar_atualizar_senha.php
+++ src/processar_atualizar_senha.php
@@ -28,24 +28,21 @@
                 $stmt = $ligacao->prepare($query_verificar_chave_recuperacao);
                 $stmt->bindParam(':chave', $chave_recuperacao);
                 $stmt ->execute();
-
-                $resultado_verificar_chave = $stmt -> fetch();
-
-                if ($resultado_verificar_chave ->rowCount() === 0) {
-                    echo "<h1>ERRO LINK ALTERADO";
-                    die();
-                }
+                $resultado_verificar_chave = $stmt ->fetch();
 
                if ($dadosAtualizarSenha) {
                     $senhaAtualizada = password_hash($dadosAtualizarSenha['SenhaAtualizada'], PASSWORD_DEFAULT);
                     $id_usuario = $resultado_verificar_chave['id_user'];
+                    $chave_recuperar_senha = 'NULL'; //para que nao fique a chave passada e nao possa recuperar
                    
                     $query_update_senha = "UPDATE usuarios 
-                                           SET senha = :senha_usuario 
+                                           SET senha = :senha_usuario, 
+                                           chave_recuperar_senha = :chave_recuperar_senha
                                            WHERE id_user = :id_user; LIMIT 1";
                     showArray($query_update_senha);
                     $preparedStmt = $ligacao->prepare($query_update_senha);
                     $preparedStmt->bindParam(':senha_usuario', $senhaAtualizada);
+                    $preparedStmt->bindParam(':chave_recuperar_senha', $chave_recuperar_senha);
                     $preparedStmt->bindParam(':id_user', $id_usuario);
                     
                     if ($preparedStmt ->execute()) {
@@ -59,12 +56,6 @@
                     
                }
 
-                
-
-                $response = [
-                    'status' => 'true',
-                    'message' => 'chave existe'
-                ];
             
             }
 
