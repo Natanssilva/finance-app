83d78b3dfba361a868528c3758ada93a43853bcc
Natanssilva <natanssilva10@gmail.com>
2023-11-07 22:08:38 -0300

    atualizando main

--- js/landing.js
+++
@@ -1,7 +0,0 @@
-//landing page
-
-// menu click
-let navbarOpen = document.querySelector('.menu');
-let menu = document.querySelector('.toggle2').addEventListener('click', function abrirMenu(){
-   navbarOpen.classList.remove('hidden');
-})
