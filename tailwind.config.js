c42c99864103b0f33fe6d9a5265b65fb0ce3f338
Natanssilva <natanssilva10@gmail.com>
2024-01-31 23:46:36 -0300

    instalando tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.php'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'custom-green': '#32632C',
+      },
+    },
+  },
+  plugins: [],
+}
